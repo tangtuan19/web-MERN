d2240f30515b4960d4f5eee917f6aa45bcef530c
tangtuan19 <tangtuan1901@gmail.com>
2024-11-16 16:33:08 +0700

    search fucntion

--- frontend/src/Pages/SearchProduct.js
+++ frontend/src/Pages/SearchProduct.js
@@ -1,25 +1,28 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useLocation, useParams } from 'react-router-dom'
 import SummaryApi from '../common'
+import VerticalCard from '../components/VerticalCard'
 
 const SearchProduct = () => {
 
 
 
     const query = useLocation()
+    const [data,setData] = useState([])
+    const [loading,setLoading] = useState(true)
 
     console.log("query",query.search)
 
 
     
     const fetchProduct = async()=>{
-        // setLoading(true)
+        setLoading(true)
         const response = await fetch(SummaryApi.searchProduct.url+query.search)
         const dataResponse = await response.json()
         console.log("dataresponse", dataResponse)
-        // setLoading(false)
+        setLoading(false)
 
-        // setData(dataResponse.data)
+        setData(dataResponse.data)
     }
 
 
@@ -28,9 +31,28 @@ const SearchProduct = () => {
     },[query])
 
   return (
-    <div>
-        SearchProduct
-        </div>
+    <div className='container mx-auto p-4'>
+    {
+      loading && (
+        <p className='text-lg text-center'>Loading ...</p>
+      )
+    }
+
+    <p className='text-lg font-semibold my-3'>Results : {data.length}</p>
+
+    {
+      data.length === 0 && !loading && (
+         <p className='bg-white text-lg text-center p-4'>No Product Found....</p>
+      )
+    }
+
+{
+        data.length !==0 && !loading && (
+          <VerticalCard loading={ loading} data={data}/>
+        )
+      }
+
+  </div>
   )
 }
 
--- frontend/src/components/VerticalCard.js
+++ frontend/src/components/VerticalCard.js
@@ -1,52 +1,21 @@
-import React, { useContext, useEffect, useRef, useState } from 'react'
-import fetchCategoryWiseProduct from '../helpers/fetchCategoryWiseProduct'
-import { FaAngleLeft, FaAngleRight } from 'react-icons/fa6'
+import React, { useContext } from 'react'
+import scrollTop from '../helpers/scrollTop'
+import addToCart from '../helpers/addToCart'
 import { Link } from 'react-router-dom'
 import Context from '../context'
 import displayUSDCurrency from '../helpers/displayCurrency'
-import addToCart from '../helpers/addToCart'
-import scrollTop from '../helpers/scrollTop'
 
-const CategroyWiseProductDisplay = ({category, heading}) => {
-    const [data,setData] = useState([])
-    const [loading,setLoading] = useState(true)
+const VerticalCard = ({loading,data = []}) => {
+
     const loadingList = new Array(13).fill(null)
 
     const { fetchUserAddToCart } = useContext(Context)
-    
-
-    
-
     const handleAddToCart = async(e,id)=>{
-       await addToCart(e,id)
-       fetchUserAddToCart()
-    }
-
-
-
-    const fetchData = async() =>{
-        setLoading(true)
-        const categoryProduct = await fetchCategoryWiseProduct(category)
-        setLoading(false)
-
-        console.log("horizontal data",categoryProduct.data)
-        setData(categoryProduct?.data)
-    }
-
-    useEffect(()=>{
-        fetchData()
-    },[])
-
-
-
-
+        await addToCart(e,id)
+        fetchUserAddToCart()
+     }
   return (
-    <div className='container mx-auto px-4 my-6 relative'>
-
-            <h2 className='text-2xl font-semibold py-4'>{heading}</h2>
-
-                
-           <div className='grid grid-cols-[repeat(auto-fit,minmax(300px,320px))] justify-between md:gap-6 overflow-x-scroll scrollbar-none transition-all'>
+    <div className='grid grid-cols-[repeat(auto-fit,minmax(300px,320px))] justify-between md:gap-6 overflow-x-scroll scrollbar-none transition-all'>
            {
 
                 loading ? (
@@ -72,7 +41,7 @@ const CategroyWiseProductDisplay = ({category, heading}) => {
                         return(
                             <Link to={"/product/"+product?._id} className='w-full min-w-[280px]  md:min-w-[320px] max-w-[280px] md:max-w-[320px]  bg-white rounded-sm shadow '  onClick={scrollTop} >
                                 <div className='bg-slate-200 h-48 p-4 min-w-[280px] md:min-w-[145px] flex justify-center items-center'>
-                                    <img src={product.productImage[0]} className='object-scale-down h-full hover:scale-110 transition-all mix-blend-multiply'/>
+                                    <img src={product?.productImage[0]} className='object-scale-down h-full hover:scale-110 transition-all mix-blend-multiply'/>
                                 </div>
                                 <div className='p-4 grid gap-3'>
                                     <h2 className='font-medium text-base md:text-lg text-ellipsis line-clamp-1 text-black'>{product?.productName}</h2>
@@ -90,10 +59,7 @@ const CategroyWiseProductDisplay = ({category, heading}) => {
                 
             }
            </div>
-            
-
-    </div>
   )
 }
 
-export default CategroyWiseProductDisplay
+export default VerticalCard
